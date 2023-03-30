27035f867d18fd95ac9373eecefb1138e535b7f9
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2023-03-30 18:22:13 +0300

    Сделал 5 лабу

--- places/places.js
+++ places/places.js
@@ -5,6 +5,16 @@ var main = function () {
     [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
     [ 1, 1, 0, 0, 1, 1, 1, 1, 1, 1 ]
   ]
+
+  var addPlaceClickListener = (li, n) => {
+    li.on("click", () => {
+      var row = li.attr('row');
+      var col = li.attr('col');
+      places[row][col] = !places[row][col];
+      $(`.tabs a:nth-child(${n}) span`).trigger("click")
+    })
+  }
+
   $(".tabs a span").toArray().forEach(element => {
     $(element).on("click", () => {
       var $element = $(element),
@@ -17,7 +27,9 @@ var main = function () {
         for (let i = 0; i < places.length; i ++){
           for (let j = 0; j < places[i].length; j ++){
             if (!places[i][j]){
-              $content.append($("<li>").text(`${String.fromCharCode(65 + i)}${j+1}`));
+              var $listElement = $("<li>").attr({ row : i, col : j}).text(`${String.fromCharCode(65 + i)}${j+1}`).addClass("place");
+              addPlaceClickListener($listElement, 1)
+              $content.append($listElement);
             }
           }
         }
@@ -26,7 +38,9 @@ var main = function () {
         for (let i = 0; i < places.length; i ++){
           for (let j = 0; j < places[i].length; j ++){
             if (places[i][j]){
-              $content.append($("<li>").text(`${String.fromCharCode(65 + i)}${j+1}`));
+              var $listElement = $("<li>").attr({ row : i, col : j}).text(`${String.fromCharCode(65 + i)}${j+1}`).addClass("place");
+              addPlaceClickListener($listElement, 2)
+              $content.append($listElement);
             }
           }
         }
@@ -35,6 +49,8 @@ var main = function () {
       return false;
     })
   })
+
+  $(".tabs a:first-child span").trigger("click")
 };
 
 $(document).ready(main);
